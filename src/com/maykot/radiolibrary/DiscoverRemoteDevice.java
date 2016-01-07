d046357cd35ea2ba31b1da31c1dbe7fcbc24c6e5
RodrigoMateus <rodrigo.m.mateus@gmail.com>
2016-01-07 12:16:17 -0200

    descobre dispositivo remoto

---
+++ src/com/maykot/radiolibrary/DiscoverRemoteDevice.java
@@ -0,0 +1,30 @@
+package com.maykot.radiolibrary;
+
+import java.io.IOException;
+
+import com.digi.xbee.api.DigiMeshDevice;
+import com.digi.xbee.api.RemoteXBeeDevice;
+import com.digi.xbee.api.XBeeNetwork;
+import com.digi.xbee.api.exceptions.XBeeException;
+import com.digi.xbee.api.utils.DeviceConfig;
+
+public class DiscoverRemoteDevice {
+
+	private static RemoteXBeeDevice remoteDevice;
+
+	public static RemoteXBeeDevice discover(DeviceConfig deviceConfig, DigiMeshDevice myDevice)
+			throws IOException, XBeeException {
+
+		// Obtain the remote XBee device from the XBee network.
+		XBeeNetwork xbeeNetwork = myDevice.getNetwork();
+		do {
+			remoteDevice = xbeeNetwork.discoverDevice(deviceConfig.getRemoteNodeID());
+			if (remoteDevice == null) {
+				System.out.println("Couldn't find the Radio " + deviceConfig.getRemoteNodeID() + ".");
+			}
+		} while (remoteDevice == null);
+		System.out.println("Was found REMOTE radio " + deviceConfig.getRemoteNodeID() + " (PowerLevel "
+				+ remoteDevice.getPowerLevel() + ").");
+		return remoteDevice;
+	}
+}
